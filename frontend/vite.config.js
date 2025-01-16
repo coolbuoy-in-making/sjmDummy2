56a01492475b065242d5d07f0dd7cec63db5e231
coolbuoy-in-making <adebayoinioluwam@example.com>
2025-01-16 09:32:09 +0100

    Upwork cloneeeee

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 5173
-  }
+    port: 5173,
+  },
+  build: {
+    outDir: 'dist', // Explicitly set the output directory
+  },
 });
