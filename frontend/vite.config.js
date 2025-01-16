f7dcd31cd8a013a3c2aaf49e1b28dd681fe55352
coolbuoy-in-making <adebayoinioluwam@example.com>
2025-01-16 10:00:37 +0100

    Upwork clonee

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-  },
-  build: {
-    outDir: 'dist', // Explicitly set the output directory
-  },
-});
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode`
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: '/',
+    plugins: [react()],
+    define: {
+      'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
+      'process.env.VITE_PYTHON_API_URL': JSON.stringify(env.VITE_PYTHON_API_URL)
+    }
+  }
+})
